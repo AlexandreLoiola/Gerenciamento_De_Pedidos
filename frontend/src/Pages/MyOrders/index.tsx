609a899461d94c43db6fc786fba06555c9cbb864
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-07-25 16:47:13 -0300

    feat: Gerar Pedido

--- frontend/src/Pages/MyOrders/index.tsx
+++ frontend/src/Pages/MyOrders/index.tsx
@@ -1,41 +1,60 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import SubmitButton from "../../Components/SubmitButton";
 import MainHeader from "../../Components/Header";
 import { FormContainer, FormInput } from "./styles";
+import axios from "axios";
+import { response } from "express";
 
 const MyOrders: React.FC = () => {
-    const [sellerCPF, setSellerCPF] = useState("");
-    const [customerCPF, setCustomerCPF] = useState("");
-                
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-    };
+  const [sellerCPF, setSellerCPF] = useState("");
+  const [customerCPF, setCustomerCPF] = useState("");
 
-    return (
-        <>
-           <MainHeader title={"Gerar Pedido"} />
-           <FormContainer>
-           <form onSubmit={handleSubmit}>
-              <FormInput
-                type="text"
-                id="sellerCPF"
-                placeholder='CPF do Vendedor'
-                value={sellerCPF}
-                onChange={(event) => setSellerCPF(event.target.value)}
-              />
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
 
-            <FormInput
-              type="text"
-              id="clientCPF"
-              placeholder='CPF do cliente'
-              value={customerCPF}
-              onChange={(event) => setCustomerCPF(event.target.value)}
-            />
-            </form>
-            <SubmitButton title={"Gerar"} />
-           </FormContainer>
-        </>
-    )
+  const handleCreate = async () => {
+    try {
+      await axios.post("http://localhost:8080/api/management/orders", {
+        "cpfCustomer": customerCPF,
+        "cpfSeller": sellerCPF
+      })
+      .then((response) => {
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.error(error.response.data.message);
+        alert(error.response.data.message)
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <MainHeader title={"Gerar Pedido"} />
+      <FormContainer>
+        <form onSubmit={handleSubmit}>
+          <FormInput
+            type="customerCpf"
+            name="customerCpf"
+            placeholder="Cpf do Cliente"
+            value={customerCPF}
+            onChange={(e) => setCustomerCPF(e.target.value)}
+          />
+          <FormInput 
+            type="sellerCpf"
+            name="sellerCpf"
+            placeholder="Cpf do Vendedor"
+            value={sellerCPF}
+            onChange={(e) => setSellerCPF(e.target.value)}
+          />
+          <SubmitButton onClick={handleCreate} title="Gerar Pedido" />
+        </form>
+      </FormContainer>
+    </>
+  );
 };
 
 export default MyOrders;
