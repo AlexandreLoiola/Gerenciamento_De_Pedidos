640e009d57e6d1712c42bd05433f4737ce7201c7
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-08-31 00:20:11 -0300

    feat: Adicionar Página Home

--- frontend/src/Pages/Home/index.tsx
+++ frontend/src/Pages/Home/index.tsx
@@ -1,17 +1,59 @@
 import React from "react";
-import MainHeader from "../../Components/Header";
-
-
-import BigButton from '../../Components/BigButton'
+import { useNavigate } from "react-router-dom";
+import { StyledButton, ArrowRightIcon } from "./styles";
+import { HeaderContainer, TitleContainer, TitleText } from "../../Components/Header/styles";
+import DividerLine from "../../Components/Header/HeaderDivisor";
+import Logout from "../../Components/Logout";
 
 const Home: React.FC = () => {
-    return (
-        <>
-            <MainHeader title={'Seja bem-vindo, Alexandre!'}/>
-            <BigButton title={'VER PRODUTOS'} redirectTo={'/catalogo-de-produtos'}/>
-            <BigButton title={'PEDIDO'} redirectTo={'/gerar-pedido'}/>
-        </>
-    )
-}
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <HeaderContainer>
+        <div style={{ width: "30%" }}></div>
+        <TitleContainer>
+          <TitleText>Seja Bem-Vindo!</TitleText>
+          <DividerLine />
+        </TitleContainer>
+        <Logout redirectTo={"/login"} />
+      </HeaderContainer>
+      <div style={{ padding: "40px 400px" }} className="d-grid gap-2">
+        <StyledButton
+          onClick={() => navigate("/gerenciar-produtos")}
+          variant="outline-success"
+          size="lg"
+        >
+          <ArrowRightIcon />
+          Gerenciar Produtos
+        </StyledButton>
+        <StyledButton
+          onClick={() => navigate("/gerenciar-funcionarios")}
+          variant="outline-success"
+          size="lg"
+        >
+          <ArrowRightIcon />
+          Gerenciar Funcionários
+        </StyledButton>
+        <StyledButton
+          onClick={() => navigate("/gerenciar-clientes")}
+          variant="outline-success"
+          size="lg"
+        >
+          <ArrowRightIcon />
+          Gerenciar Clientes
+        </StyledButton>
+        <StyledButton
+          onClick={() => navigate("/gerenciar-pedidos")}
+          variant="outline-success"
+          size="lg"
+        >
+          <ArrowRightIcon />
+          Gerenciar Pedidos
+        </StyledButton>
+      </div>
+    </>
+  );
+};
 
 export default Home;
--- frontend/src/Pages/Home/styles.ts
+++ frontend/src/Pages/Home/styles.ts
@@ -1,1 +1,19 @@
 import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
+import {MdOutlineKeyboardArrowRight} from "react-icons/md"
+
+export const StyledButton = styled(Button)`
+    font-weight: 600;
+    padding: 12px;
+    padding-left: 30px;
+    margin-bottom: 10px;
+    text-align: left;
+    position: relative;
+    border: 1px solid;
+`
+
+export const ArrowRightIcon = styled(MdOutlineKeyboardArrowRight)`
+    position: absolute;
+    right: 20px;
+    font-size: 32px;
+`
