576bfd6d92e4f86405a564a144177bc9f05eb23a
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-09-14 19:12:01 -0300

    feat: Adiciona mensagens de validação

--- backend/src/main/java/com/alexandreloiola/salesmanagement/rest/form/ProductUpdateForm.java
+++ backend/src/main/java/com/alexandreloiola/salesmanagement/rest/form/ProductUpdateForm.java
@@ -9,28 +9,28 @@ import java.math.BigDecimal;
 @Data
 public class ProductUpdateForm {
 
-    @NotEmpty
+    @NotEmpty(message = "O campo nome não pode ser vazio")
     @NotBlank(message = "O campo nome não pode ficar em branco.")
-    @Size(max = 128)
+    @Size(max = 100)
     private String name;
 
-    @NotEmpty
-    @NotBlank(message = "O campo nome não pode ficar em branco.")
-    @Size(max = 256)
+    @NotEmpty(message = "O campo descrição não pode ser vazio")
+    @NotBlank(message = "O campo descrição não pode ficar em branco.")
+    @Size(max = 200)
     private String description;
 
-    @NotNull
+    @NotNull(message = "O campo preço unitário não pode ser vazio")
     @DecimalMax(value = "99999.99")
     @Digits(integer = 16, fraction = 2)
-    @DecimalMin(value = "0.00")
+    @DecimalMin(value = "0.01")
     private BigDecimal unitPrice;
 
     @NotNull(message ="O campo de quantidade não pode ficar vazio")
     @DecimalMax(value = "99999")
-    @DecimalMin(value = "-99999")
+    @DecimalMin(value = "0")
     private Integer stockQuantity;
 
-    @NotNull
+    @NotNull(message = "O campo status não pode ficar vazio")
     @BooleanFlag
     private Boolean isActive;
 
