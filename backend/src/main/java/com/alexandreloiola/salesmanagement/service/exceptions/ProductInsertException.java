a11bc4218937e2d4b7929a21a33471534afd2837
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-09-14 19:09:23 -0300

    feat: Adiciona arquivo ProductInsertException

---
+++ backend/src/main/java/com/alexandreloiola/salesmanagement/service/exceptions/ProductInsertException.java
@@ -0,0 +1,13 @@
+package com.alexandreloiola.salesmanagement.service.exceptions;
+
+public class ProductInsertException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ProductInsertException(String msg) {
+        super(msg);
+    }
+
+    public ProductInsertException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
