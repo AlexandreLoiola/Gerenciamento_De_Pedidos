b4c239a85f46a0f1410eb546cd43ad7b2c2e297f
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-09-14 19:11:02 -0300

    feat: Adiciona exceções de Product

--- backend/src/main/java/com/alexandreloiola/salesmanagement/rest/controller/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/alexandreloiola/salesmanagement/rest/controller/exceptions/ControllerExceptionHandler.java
@@ -1,66 +1,72 @@
 package com.alexandreloiola.salesmanagement.rest.controller.exceptions;
 
-import com.alexandreloiola.salesmanagement.service.exceptions.BusinessRuleException;
-import com.alexandreloiola.salesmanagement.service.exceptions.ConstraintException;
-import com.alexandreloiola.salesmanagement.service.exceptions.DataIntegrityException;
-import com.alexandreloiola.salesmanagement.service.exceptions.ObjectNotFoundException;
+import com.alexandreloiola.salesmanagement.service.exceptions.*;
+
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(BusinessRuleException.class)
-    public ResponseEntity<StandardError> businessRule(BusinessRuleException error, HttpServletRequest request) {
-        StandardError err = new StandardError(
-                                    System.currentTimeMillis(),
-                                    HttpStatus.CONFLICT.value(),
-                              "Regra de negócio",
-                                    error.getMessage(),
-                                    request.getRequestURI()
-        );
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionsDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+        String errorMsg = ex.getBindingResult().getFieldError().getDefaultMessage();
+        ExceptionsDto exceptionsDto = new ExceptionsDto(
+                System.currentTimeMillis(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Requisição Inválida",
+                errorMsg,
+                request.getRequestURI());
+        return new ResponseEntity<>(exceptionsDto, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(ConstraintException.class)
-    public ResponseEntity<StandardError> constraint(ConstraintException error, HttpServletRequest request) {
-        StandardError err = new StandardError(
-                                    System.currentTimeMillis(),
-                                    HttpStatus.BAD_REQUEST.value(),
-                                "Restrição de dados",
-                                    error.getMessage(),
-                                    request.getRequestURI()
-        );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ExceptionsDto> handleProductNotFoundException(ProductNotFoundException ex, HttpServletRequest request) {
+        ExceptionsDto exceptionsDto = new ExceptionsDto(
+                System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(),
+                "Não encontrado",
+                ex.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(exceptionsDto, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(DataIntegrityException.class)
-    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
-        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Integridade de dados", e.getMessage(), request.getRequestURI());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    @ExceptionHandler(ProductAlreadyExistsException.class)
+    public ResponseEntity<ExceptionsDto> handleProductAlreadyExistsException(ProductAlreadyExistsException ex, HttpServletRequest request) {
+        ExceptionsDto exceptionsDto = new ExceptionsDto(
+                System.currentTimeMillis(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Requisição inválida",
+                ex.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(exceptionsDto, HttpStatus.BAD_REQUEST);
     }
 
-
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<StandardError> handleDataIntegrityViolation(DataIntegrityViolationException e, HttpServletRequest request) {
-        StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "Integridade de dados", e.getMessage(), request.getRequestURI());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    @ExceptionHandler(ProductInsertException.class)
+    public ResponseEntity<ExceptionsDto> handleProductInsertException(ProductInsertException ex, HttpServletRequest request) {
+        ExceptionsDto exceptionsDto = new ExceptionsDto(
+                System.currentTimeMillis(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro interno no servidor",
+                ex.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(exceptionsDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-
-    @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError> ObjectNotFound(ObjectNotFoundException error, HttpServletRequest request) {
-        StandardError err = new StandardError(
-                                    System.currentTimeMillis(),
-                                    HttpStatus.NOT_FOUND.value(),
-                                "Não encontrado.",
-                                    error.getMessage(),
-                                    request.getRequestURI()
-        );
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    @ExceptionHandler(ProductUpdateException.class)
+    public ResponseEntity<ExceptionsDto> handleProductUpdateException(ProductUpdateException ex, HttpServletRequest request) {
+        ExceptionsDto exceptionsDto = new ExceptionsDto(
+                System.currentTimeMillis(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "Erro interno no servidor",
+                ex.getMessage(),
+                request.getRequestURI());
+        return new ResponseEntity<>(exceptionsDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
