e30989ddb7239c4466fb1dcd6ac21a6de4e6a5a7
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-09-15 23:26:43 -0300

    refact: Refatora nome dos métodos e os reorganiza em OrderStatusService

--- backend/src/main/java/com/alexandreloiola/salesmanagement/service/OrderStatusService.java
+++ backend/src/main/java/com/alexandreloiola/salesmanagement/service/OrderStatusService.java
@@ -5,16 +5,18 @@ import com.alexandreloiola.salesmanagement.repository.OrderStatusRepository;
 import com.alexandreloiola.salesmanagement.rest.dto.OrderStatusDto;
 import com.alexandreloiola.salesmanagement.rest.form.OrderStatusForm;
 import com.alexandreloiola.salesmanagement.rest.form.OrderStatusUpdateForm;
-import com.alexandreloiola.salesmanagement.service.exceptions.DataIntegrityException;
-import com.alexandreloiola.salesmanagement.service.exceptions.ObjectNotFoundException;
+import com.alexandreloiola.salesmanagement.service.exceptions.orderStatus.OrderStatusAlreadyExistsException;
+import com.alexandreloiola.salesmanagement.service.exceptions.orderStatus.OrderStatusInsertException;
+import com.alexandreloiola.salesmanagement.service.exceptions.orderStatus.OrderStatusNotFoundException;
+import com.alexandreloiola.salesmanagement.service.exceptions.orderStatus.OrderStatusUpdateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+
 
 @Service
 public class OrderStatusService {
@@ -26,73 +28,70 @@ public class OrderStatusService {
         return convertModelListToDtoList(orderStatusModelist);
     }
 
-    public OrderStatusDto getOrderStatusById(Long id) {
-        try {
-            OrderStatusModel orderStatusModel = orderStatusRepository.findById(id).get();
-            return convertModelToDto(orderStatusModel);
-        } catch(NoSuchElementException err) {
-            throw new ObjectNotFoundException("Status de pedido não encontrado!");
-        }
+    private OrderStatusModel findOrderStatusByDescription(String orderDescription) {
+        return orderStatusRepository.findByDescription(orderDescription)
+                .orElseThrow(() -> new OrderStatusNotFoundException(
+                        String.format("O status '%s' não foi encontrado", orderDescription))
+                );
     }
 
+    public OrderStatusDto getOrderStatusByDescription(String orderDescription) {
+        OrderStatusModel orderStatusModel = findOrderStatusByDescription(orderDescription);
+        return convertModelToDto(orderStatusModel);
+    }
+
+    @Transactional
     public OrderStatusDto insertOrderStatus(OrderStatusForm orderStatusForm) {
+        if (orderStatusRepository.findByDescription(orderStatusForm.getDescription()).isPresent()) {
+            throw new OrderStatusAlreadyExistsException(
+                    String.format("O status '%s' já está cadastrado", orderStatusForm.getDescription())
+            );
+        }
         try {
-            Optional<OrderStatusModel> byDescription = orderStatusRepository.findByDescription(orderStatusForm.getDescription());
-            if (byDescription.isPresent()) {
-                throw new DataIntegrityException("Essa descrição já foi cadastrado");
-            }
             OrderStatusModel orderStatusModel = convertFormToModel(orderStatusForm);
             orderStatusModel.setIsActive(true);
             orderStatusModel = orderStatusRepository.save(orderStatusModel);
-
             return convertModelToDto(orderStatusModel);
-        }  catch (DataIntegrityViolationException err) {
-            throw new DataIntegrityViolationException("Campo(s) obrigatório(s) do Status de Pedido não foi(foram) devidamente preenchido(s).");
+        } catch (DataIntegrityViolationException err) {
+            throw new OrderStatusInsertException(
+                    String.format("Falha ao cadastrar o status '%s'. Verifique os dados.", orderStatusForm.getDescription())
+            );
         }
     }
 
-    public OrderStatusDto updateOrderStatus(Long id, OrderStatusUpdateForm orderStatusUpdateForm) {
+    @Transactional
+    public OrderStatusDto updateOrderStatus(String orderStatusDescription, OrderStatusUpdateForm orderStatusUpdateForm) {
+        OrderStatusModel orderStatusModel = findOrderStatusByDescription(orderStatusDescription);
         try {
-            Optional<OrderStatusModel> orderStatusModel = orderStatusRepository.findById(id);
-            if (orderStatusModel.isPresent()) {
-                OrderStatusModel orderStatusUpdate = orderStatusModel.get();
-                orderStatusUpdate.setDescription(orderStatusUpdateForm.getDescription());
-                orderStatusUpdate.setIsActive(orderStatusUpdateForm.getIsActive());
-                orderStatusUpdate = orderStatusRepository.save(orderStatusUpdate);
-                return convertModelToDto(orderStatusUpdate);
-            }  else {
-                throw new DataIntegrityViolationException("Campo(s) obrigatório(s) do produto não foi(foram) devidamente preenchido(s).");
-            }
+            OrderStatusModel orderStatusUpdate = orderStatusModel;
+            orderStatusUpdate.setDescription(orderStatusUpdateForm.getDescription());
+            orderStatusUpdate.setIsActive(orderStatusUpdateForm.getIsActive());
+            orderStatusUpdate = orderStatusRepository.save(orderStatusUpdate);
+            return convertModelToDto(orderStatusUpdate);
         } catch (DataIntegrityViolationException err) {
-            throw new DataIntegrityViolationException("Esse produto não está cadastrado");
+            throw new OrderStatusUpdateException(
+                    String.format("Falha ao atualizar o produto %s. Verifique se os dados estão corretos", orderStatusUpdateForm.getDescription())
+            );
         }
     }
 
-    public void deleteOrderStatus(Long id) {
-        try {
-            if (orderStatusRepository.existsById(id)) {
-                orderStatusRepository.deleteById(id);
-            }
-        } catch (DataIntegrityViolationException err) {
-            throw new DataIntegrityViolationException("Não foi possível deletar o Status do Pedido");
-        }
+    @Transactional
+    public void deleteOrderStatus(String orderDescription) {
+        OrderStatusModel orderStatusModel = findOrderStatusByDescription(orderDescription);
+        Long orderStatusId = orderStatusModel.getId();
+        orderStatusRepository.deleteById(orderStatusId);
     }
 
     public OrderStatusModel convertFormToModel(OrderStatusForm orderStatusForm) {
         OrderStatusModel orderStatusModel = new OrderStatusModel();
-
         orderStatusModel.setDescription(orderStatusForm.getDescription());
-
         return orderStatusModel;
     }
 
     public OrderStatusDto convertModelToDto(OrderStatusModel orderStatusModel) {
         OrderStatusDto orderStatusDto = new OrderStatusDto();
-
-        orderStatusDto.setId(orderStatusModel.getId());
         orderStatusDto.setDescription(orderStatusModel.getDescription());
         orderStatusDto.setIsActive(orderStatusModel.getIsActive());
-
         return orderStatusDto;
     }
 
