7dd73faa335038a4531c3da20a439df756ccbe25
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-09-16 22:41:01 -0300

    refact: Renomeia atributos de OrderMOdel

--- backend/src/main/java/com/alexandreloiola/salesmanagement/model/OrderModel.java
+++ backend/src/main/java/com/alexandreloiola/salesmanagement/model/OrderModel.java
@@ -14,25 +14,25 @@ public class OrderModel {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "code_order", length = 10, nullable = false, unique = true)
+    @Column(name = "order_number", length = 10, nullable = false, unique = true)
     private Long orderNumber;
-
-    @Column(name = "total_price", precision = 10, scale = 2 , nullable = false)
-    private BigDecimal totalPrice;
 
     @Column(name = "data_time", nullable = false)
     private LocalDateTime dataTime;
 
+    @Column(name = "total_price", precision = 10, scale = 2 , nullable = false)
+    private BigDecimal totalPrice;
+
     @OneToOne
     @JoinColumn(name = "id_status", referencedColumnName = "id")
-    private OrderStatusModel status;
+    private OrderStatusModel idStatus;
 
     @ManyToOne
-    @JoinColumn(name = "customer_id", referencedColumnName = "id")
-    private PersonModel customer;
+    @JoinColumn(name = "id_customer", referencedColumnName = "id")
+    private PersonModel idCustomer;
 
     @ManyToOne
-    @JoinColumn(name = "employee_id", referencedColumnName = "id")
-    private PersonModel seller;
+    @JoinColumn(name = "id_employee", referencedColumnName = "id")
+    private PersonModel idSeller;
 
 }
