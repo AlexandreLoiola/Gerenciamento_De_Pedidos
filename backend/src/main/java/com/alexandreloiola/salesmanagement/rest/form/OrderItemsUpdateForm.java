ebcd9553c8b4c5fd8be5f10b6c3c171382962cb6
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-09-19 22:32:18 -0300

    refact: reescreve mensagens de validação

--- backend/src/main/java/com/alexandreloiola/salesmanagement/rest/form/OrderItemsUpdateForm.java
+++ backend/src/main/java/com/alexandreloiola/salesmanagement/rest/form/OrderItemsUpdateForm.java
@@ -6,7 +6,9 @@ import lombok.Data;
 @Data
 public class OrderItemsUpdateForm {
 
-    @NotNull
+    @NotNull(message = "A quantidade não pode ser nula")
+    @Min(value = 1, message = "A quantidade deve ser pelo menos 1")
+    @Max(value = 99999, message = "A quantidade não pode exceder 99999")
     private int quantity;
 
 }
