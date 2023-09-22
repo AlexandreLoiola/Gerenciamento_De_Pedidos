2bf46ac785fbaa2ed180b22710aac2e59d2d46a7
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-09-21 22:48:07 -0300

    feat: Adiciona estilos modal de visualização do pedido

---
+++ frontend/src/Components/Modal/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import Modal from 'react-bootstrap/Modal';
+
+export const StyledModal = styled(Modal)`
+  .modal-content {
+    background-color: #f8f9fa;
+    color: #343a40;
+  }
+`;
