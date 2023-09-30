b5c68e64d32869f6791dcbbfa5ab1de10777d11a
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2023-09-30 18:26:13 -0300

    refact: Supote a variável de ambiente

--- backend/src/main/java/com/alexandreloiola/salesmanagement/SalesManagementApplication.java
+++ backend/src/main/java/com/alexandreloiola/salesmanagement/SalesManagementApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SalesManagementApplication {
+	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesManagementApplication.class, args);
